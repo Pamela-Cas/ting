d058abb4ab0f250d7f9cc8e3dc74ddfad40a6d5c
Pamela-Cas <pxcx611@gmail.com>
2023-11-07 12:29:37 -0300

    ajustes lint

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,23 +5,22 @@ def process(path_file, instance):
     for index in range(len(instance)):
         if instance.search(index)["nome_do_arquivo"] == path_file:
             return None
-
     file = txt_importer(path_file)
-
     data = {
             "nome_do_arquivo": path_file,
             "qtd_linhas": len(file),
             "linhas_do_arquivo": (file),
         }
-
     instance.enqueue(data)
-
     sys.stdout.write(str(data)) 
 
 def remove(instance):
     if len(instance) == 0:
         print ("Não há elementos")
         return
+    delete = instance.dequeue()["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {delete} removido com sucesso\n")
+
     
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
